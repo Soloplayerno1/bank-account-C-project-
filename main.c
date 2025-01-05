4a6b81a4358e5a5994c4da121b99a276eb6aa069
Admin <soloplayerno1@gmail.com>
2025-01-05 22:00:48 +0700

    day1 end day update( get stuck )

--- main.c
+++ main.c
@@ -3,12 +3,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-//void Login();
-void clear_input_buffer();
-void Account();
-bool Login();
-
-
 #define MAX 26
 
 typedef struct{
@@ -16,16 +10,26 @@ typedef struct{
     char password[MAX];
     long balance;
 }account;
+//void Login();
+void clear_input_buffer();
+void Account();
+void Login(account *user);
+void inside_Account(account *user);
+
 
 int main(){
-    int choose = 0; 
+    int choose = 0;
+    account user; 
     while(choose != 3)
     {
         printf("1.Login\n2.Create account\n3.Exit\n");
         scanf("%d", &choose);
         clear_input_buffer();
         if(choose == 1)
-            Login();
+        {
+            Login(&user);
+            inside_Account(&user);
+        }
         if(choose == 2)
             Account();
         if(choose == 3)
@@ -33,6 +37,12 @@ int main(){
     }
 }
 
+void inside_Account(account* user){
+    if(user == NULL)
+        return;
+    printf("Username: %s\n", user->name);
+    printf("Current balance: %ld\n", user->balance);
+}
 
 void get_User_Name(char* username, FILE *name){
     printf("User name: ");
@@ -91,13 +101,12 @@ void clear_input_buffer(){
 }
 
 
-bool Login(){
-    account user;
+void Login(account *user){
     printf("User name: ");
-    fgets(user.name, 26, stdin);
-    user.name[strcspn(user.name, "\n")] = '\0';
+    fgets(user->name, 26, stdin);
+    user->name[strcspn(user->name, "\n")] = '\0';
     printf("Password: ");
-    scanf("%s", &user.password);
+    scanf("%s", &user->password);
     FILE *f = fopen("user.txt", "r");
     char line[256];
     fgets(line,sizeof(line), f);
@@ -121,13 +130,24 @@ bool Login(){
             i++; p++;
         }
         password[p] = '\0';
-        if(!strcmp(user.name, username) && !strcmp(user.password, password))
+        if(!strcmp(user->name, username) && !strcmp(user->password, password))
         {
+            i+=2;
+            int b = 0;
+            char balance[50];
+            while(line[i]!='\0')
+            {
+                balance[b] = line[i];
+                b++; i++;
+            }
+            user->balance = atol(balance);
             printf("Login success.\n");
-            return true;
+            account* User = (account*)malloc(sizeof(account));
+            return;
         }
     }
     fclose(f);
+    user = NULL;
     printf("Incorrect username or password.\n");
-    return false;
+    return ;
 }
